502c328479e671bbd89e684bcf46e0eeb528f2e3
cyoun <cdesyoun@gmail.com>
2015-10-01 15:39:29 -0700

    update codes

--- wof/flask/views/rest.py
+++ wof/flask/views/rest.py
@@ -153,7 +153,7 @@ def get_values_wml2():
         firstMethod = data_values[0].MethodID
         method_result = current_app.wof_inst.dao.get_method_by_id(firstMethod)
 
-    current_date = str(datetime.datetime.now())
+    current_date = str(datetime.datetime.now().isoformat())
 
     response = make_response(render_template('wml2_values_template.xml',
                                             current_date=current_date,
