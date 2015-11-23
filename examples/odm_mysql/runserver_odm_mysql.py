fb6c5e9c5b57561af3854d7c72c65ba2f684a8df
Russ Nelson <russnelson@gmail.com>
2015-11-23 12:39:36 -0500

    make connection a required file parameter.

--- examples/odm_mysql/runserver_odm_mysql.py
+++ examples/odm_mysql/runserver_odm_mysql.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 
 import logging
 
@@ -24,9 +25,10 @@ import sys
 logging.basicConfig(level=logging.DEBUG)
 
 
-def startServer(config='config.cfg',connection=None,openPort=8080):
+def startServer(connection, config='config.cfg',openPort=8080):
+"""given an open file on connection, read it to get a connection string to open the database and start up flask running WOF."""
 
-    dao = OdmDao(connection)
+    dao = OdmDao(connection.read())
     app = wof.create_wof_flask_app(dao, config)
     app.config['DEBUG'] = True
 
@@ -40,7 +42,7 @@ def startServer(config='config.cfg',connection=None,openPort=8080):
 
     print "----------------------------------------------------------------"
     print "----------------------------------------------------------------"
-    print "HTML Acess Service endpoints at "
+    print "HTML Access Service endpoints at "
     for path in wof.site_map(app):
         print "%s%s" % (url,path)
 
@@ -52,14 +54,14 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='start WOF for an ODM1 database.')
-    parser.add_argument('--config',
+    parser.add_argument('connection', type=argparse.FileType('r'), 
+                       help='The name of a file containing the Connection String eg: mysql://username:password@localhost/database')
+    parser.add_argument('--config', default="config.cfg",
                        help='Configuration file')
-    parser.add_argument('--connection',
-                       help='Connection String eg: "mssql+pyodbc://username:password@localhost/LittleBear11?driver=SQL+Server+Native+Client+10.0"')
-
     parser.add_argument('--port',
                        help='Open port for server."', default=8080, type=int)
     args = parser.parse_args()
     print(args)
 
-    startServer(config=args.config,connection=args.connection,openPort=args.port)
+    startServer(connection=args.connection,config=args.config,openPort=args.port)
+
