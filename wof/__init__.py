8434a807c3fd6fb9d097f7d2029b59f361bc037d
valentinedwv <david.valentine@gmail.com>
2015-11-24 12:54:52 -0800

    Update version

--- wof/__init__.py
+++ wof/__init__.py
@@ -1,5 +1,5 @@
 # The version as used in the setup.py
-__version__ = "2.0.0005-alpha"
+__version__ = "2.0.0006-alpha"
 
 from core import create_wof_flask_app, _SERVICE_PARAMS, site_map
 from core_1_0 import WOF
