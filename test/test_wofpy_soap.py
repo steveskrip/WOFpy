c6e4c52f6464fd9bdecbd471dd3352334bc9aa71
David Valentine <david.valentine@gmail.com>
2016-04-18 15:50:54 -0700

    nosetest -v so we can see what tests are run

--- test/test_wofpy_soap.py
+++ test/test_wofpy_soap.py
@@ -48,15 +48,15 @@ try:
     req = requests.get('http://127.0.0.1:8080/twdb/soap/cuahsi_1_0/.wsdl', timeout=2)
     if req.status_code == 200:
         serverUp = True
-        print "found server"
+        serverMessage= "found server"
     else:
         serverUp = False
-        print "found server, but URL failed"
+        serverMessage = "found server, but URL failed"
 except:
     serverUp = False
-    print "URL failed"
+    serverMessage = "No server running"
 
-@unittest.skipUnless(serverUp, "No server running")
+@unittest.skipUnless(serverUp, serverMessage)
 class TestWofpySoap(unittest.TestCase):
     """
     UnitTest to test the WOF SOAP methods using a Suds client.
