533f6d3bffdf26b987da7c2e8b3a45c0107759cf
lsetiawan <landungs@uw.edu>
2017-05-04 12:53:11 -0700

    Updated production_configs

--- wof/examples/production_configs/wsgi.py
+++ wof/examples/production_configs/wsgi.py
@@ -1,6 +1,3 @@
 #! /home/ubuntu/miniconda/envs/wofpy/bin/python
-import sys
-sys.path.insert(0, "/var/www/timeseries")
-
 from runserver import app as application
 application.secret_key = 'Thisismysecretkey'
